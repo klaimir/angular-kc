aa7406e7f2b11686d68ce8a457e80a16e9b07b46
ANGEL LUIS BERASUAIN RUIZ <0015523@viewnext.com>
2021-03-30 11:57:04 +0200

    Filtrar tareas en control de tareas forma imperativa

--- src/app/tareas/lista/lista.component.ts
+++ src/app/tareas/lista/lista.component.ts
@@ -13,51 +13,15 @@ import {
   styleUrls: ['./lista.component.css'],
 })
 export class ListaComponent implements OnInit {
-  @Input() taskList: string[];
+  @Input() taskList: any[];
   @Output() evtDelete: EventEmitter<number>;
-
-  filter: string;
-  filteredTaskList: any[];
-
-  // INFO: Si creamos un getter que tenga las tareas mapeadas en todo momento, angular intenta calcularlo todo el rato y renderizarlo,
-  // ya que, está escuchando cambios en taskList al ser un parámetro de entrada. Por tanto, es mejor tener un método de mapeo que se aplique
-  // una única vez después del filtrado de tareas.
-  _mappingTask(taskList: string[]): any[] {
-    return taskList.map((task, index) => {
-      return {
-        index: index,
-        taskName: task,
-      };
-    });
-  }
-
-  filterTaskList() {
-    if (this.filter) {
-      this.filteredTaskList = this._mappingTask(
-        this.taskList.filter((task) => {
-          return task.includes(this.filter);
-        })
-      );
-    } else {
-      this.filteredTaskList = this._mappingTask(this.taskList);
-    }
-  }
 
   btnDelete(position: number) {
     this.evtDelete.emit(position);
-    this.filter = '';
   }
 
   constructor() {
     this.evtDelete = new EventEmitter();
-    this.filter = '';
-    this.filteredTaskList = [];
-  }
-
-  ngOnChanges(changes: SimpleChanges) {
-    debugger;
-    this.filter = '';
-    this.filterTaskList();
   }
 
   ngOnInit(): void {}
--- src/app/tareas/tareas.component.ts
+++ src/app/tareas/tareas.component.ts
@@ -7,14 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TareasComponent implements OnInit {
 
-  taskList: string [];
+  filter: string;
+  filteredTaskList: any[];
+
   errorMessage: string;
   succesMessage: string;
+
+  taskList: string [];
 
   constructor() { }
 
   ngOnInit(): void {
     this.taskList = [];
+    this.filteredTaskList = [];
+    this.filter = '';
   }
 
   addTareas(itemName: string) {
@@ -22,6 +28,7 @@ export class TareasComponent implements OnInit {
     this.errorMessage = '';
     if(itemName) {
       this.taskList.push(itemName);
+      this.filterTaskList();
       this.succesMessage = 'Tarea insertada con éxito';
       setTimeout(() => {
         this.succesMessage = '';
@@ -34,12 +41,30 @@ export class TareasComponent implements OnInit {
   deleteTask(position: number) {
     // INFO: Pasamos la posición porque al calcularla podría eliminar érroneamente elementos repetidos
     this.taskList.splice(position, 1);
+    this.filterTaskList();
     this.succesMessage = 'Tarea eliminada con éxito';
     setTimeout(() => {
       this.succesMessage = '';
     }, 1500);
   }
 
-  // TODO: Probar a pasar la lógica de filtrado aquí
+  get taskListMapped(): any[] {
+    return this.taskList.map((task, index) => {
+      return {
+        index: index,
+        taskName: task,
+      };
+    });
+  }
+
+  filterTaskList() {
+    if (this.filter) {
+      this.filteredTaskList = this.taskListMapped.filter((task) => {
+          return task.taskName.includes(this.filter);
+        })
+    } else {
+      this.filteredTaskList = this.taskListMapped;
+    }
+  }
 
 }
